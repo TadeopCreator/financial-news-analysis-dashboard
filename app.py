cb3778f6db7bccfc1f34fca411cad2ab073ad449
Tadeo Deluca <tadeo.deluca2002@gmail.com>
2024-01-07 22:57:57 -0300

    News listing and details

--- app.py
+++ app.py
@@ -7,33 +7,32 @@ from google.cloud import firestore
 news_list = []
 selected_new_index = -1
 selected_new_info = {}
+selected_page = 1
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    global news_list
+    global news_list, selected_page
+
+    page = int(request.args.get('page', 1))
+    if page < 1:
+        page = 1
+    selected_page = page
+
     news_list = []    
 
-    df = px.data.gapminder().query("country=='Canada'")
-    fig = px.line(df, x="year", y="lifeExp", title='Life expectancy in Canada')
-
-    # Configuración del tema oscuro
-    fig.update_layout(
-        plot_bgcolor='#212529',  # Color de fondo del gráfico
-        paper_bgcolor='#212529', # Color de fondo del área del papel
-        font=dict(color='white') # Cambio del color de la fuente para mejorar la visibilidad
-    )
-
-    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
-
-    return render_template('index.html', graphJSON=graphJSON)
+    return render_template('index.html', current_page=selected_page)
 
 @app.route('/get_news', methods=['GET'])
 def obtener_noticias():
+    global selected_page
 
-    db = firestore.Client(project='financial-news-analysis-410223')    
-    docs = db.collection("news").order_by('time_published', direction=firestore.Query.DESCENDING).limit(20).stream()
+    db = firestore.Client(project='financial-news-analysis-410223')
+    if selected_page != 1:
+        docs = db.collection("news").order_by('time_published', direction=firestore.Query.DESCENDING).offset(20*(selected_page-1)).limit(20).stream()
+    else:
+        docs = db.collection("news").order_by('time_published', direction=firestore.Query.DESCENDING).limit(20).stream()
 
     for doc in docs:
         news_list.append(doc.to_dict())
@@ -42,29 +41,39 @@ def obtener_noticias():
     return news_list
 
 
-@app.route('/new_analysis/<int:index>')
-def new_analysis(index):
+@app.route('/new/<int:index>')
+def new(index):
     global news_list, selected_new_index, selected_new_info
     selected_new_index = index-1
 
     if (news_list):
         selected_new_info = news_list[selected_new_index]
 
-    return render_template('new_analysis.html', new=selected_new_info)
+    return render_template('new.html', new=selected_new_info)
 
 # @app.route('/save_new', methods=['POST'])
 # def save_new():
 #     data = request.form['data']
 
-#     return render_template('new_analysis.html', new=selected_new_info, index=selected_new_index, message=data)
+#     return render_template('new.html', new=selected_new_info, index=selected_new_index, message=data)
 
-@app.route('/market_overview')
-def market_overview():
-    return render_template('market_overview.html')
+@app.route('/news_insights')
+def news_insights():
+        
+    df = px.data.gapminder().query("country=='Canada'")
+    fig = px.line(df, x="year", y="lifeExp", title='Life expectancy in Canada')
+
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+
+    return render_template('news_insights.html', graphJSON=graphJSON)
 
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/portfolio')
+def portfolio():
+    return render_template('portfolio.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
